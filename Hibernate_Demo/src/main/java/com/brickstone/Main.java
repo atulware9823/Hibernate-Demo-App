748ddb2b82f0aab7be83692f12339c6a539d9a71
Atul Ware <eratulware@gmail.com>
2025-08-13 15:19:58 +0530

    Hibernet connected to DB

--- Hibernate_Demo/src/main/java/com/brickstone/Main.java
+++ Hibernate_Demo/src/main/java/com/brickstone/Main.java
@@ -1,7 +1,24 @@
 package com.brickstone;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setSid(1);
+        s1.setSname("Atul");
+        s1.setTech("Java");
 
+        Configuration config = new Configuration();
+        config.addAnnotatedClass(com.brickstone.Student.class);
+        config.configure();
+        SessionFactory sf = config.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.persist(s1);
+        transaction.commit();
     }
 }
